904de27f1a15b650c09874af2f3c8db76f6424d1
Deepti Singh <vickyofficialkumar98@gmail.com>
2025-05-05 06:19:23 +0530

    improved login page structure

--- src/app/auth/login/loginForm.tsx
+++ src/app/auth/login/loginForm.tsx
@@ -3,7 +3,6 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useRouter } from "next/navigation";
-import Image from "next/image";
 import { Formik } from "formik";
 
 import { login, logout } from "@/app/lib/features/auth/authSlice";
@@ -52,71 +51,48 @@ const LoginForm = () => {
 		dispatch(stopLoading());
 	};
 	return (
-		<section className='w-full h-screen flex overflow-y-hidden'>
-			<div className='w-[60%] h-full xl:flex hidden'>
-				<div className="w-full h-full bg-[url('/assets/login-bg.jpeg')] bg-cover bg-center"></div>
-			</div>
-			<div className='xl:w-[40%] w-full bg-white flex items-center justify-center overflow-y-scroll'>
-				<div className='sm:w-[70%] w-[90%] flex flex-col items-start'>
-					<Image src={"/assets/logo.svg"} width={60} height={60} alt='logo' />
-					<p className='mt-3 sm:text-[32px] text-[24px] font-bold text-[#525252]'>
-						Login to your Account
-					</p>
-					<p className='mt-1 text-base font-normal text-[#525252]'>
-						See what is going on with your Workboard
-					</p>
-					<Formik
-						initialValues={{ username: "", password: "" }}
-						validationSchema={LoginSchema}
-						onSubmit={(values) => {
-							handleLogin(values);
-						}}>
-						{({
-							values,
-							handleSubmit,
-							handleChange,
-							handleBlur,
-							errors,
-							touched,
-						}) => (
-							<form onSubmit={handleSubmit} className='w-full'>
-								<InputField
-									label='username'
-									type={"username"}
-									name={"username"}
-									value={values.username}
-									placeholder={"Enter your username"}
-									handleChange={handleChange}
-									handleBlur={handleBlur}
-									error={errors.username}
-									touched={touched.username}
-									required
-									cls='mt-12'
-									max={20}
-								/>
-								<PasswordField
-									label='Password'
-									name='password'
-									value={values.password}
-									placeholder='Enter your password'
-									handleChange={handleChange}
-									handleBlur={handleBlur}
-									error={errors.password}
-									touched={touched.password}
-									required
-									cls='mt-4'
-									max={20}
-								/>
+<Formik
+	initialValues={{ username: "", password: "" }}
+	validationSchema={LoginSchema}
+	onSubmit={(values) => {
+		handleLogin(values);
+	}}>
+	{({ values, handleSubmit, handleChange, handleBlur, errors, touched }) => (
+		<form onSubmit={handleSubmit} className='w-full'>
+			<InputField
+				label='username'
+				type={"username"}
+				name={"username"}
+				value={values.username}
+				placeholder={"Enter your username"}
+				handleChange={handleChange}
+				handleBlur={handleBlur}
+				error={errors.username}
+				touched={touched.username}
+				required
+				cls='mt-12'
+				max={20}
+			/>
+			<PasswordField
+				label='Password'
+				name='password'
+				value={values.password}
+				placeholder='Enter your password'
+				handleChange={handleChange}
+				handleBlur={handleBlur}
+				error={errors.password}
+				touched={touched.password}
+				required
+				cls='mt-4'
+				max={20}
+			/>
 
-								<button className='mt-12 w-full rounded-md bg-[#7F265B] h-[50px] flex items-center justify-center text-white font-extrabold cursor-pointer'>
-									Login
-								</button>
-							</form>
-						)}
-					</Formik>
-				</div>
-			</div>
-		</section>
+			<button className='mt-12 w-full rounded-md bg-[#7F265B] h-[50px] flex items-center justify-center text-white font-extrabold cursor-pointer'>
+				Login
+			</button>
+		</form>
+	)}
+</Formik>
 	);
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,24 @@
+import Image from "next/image";
 import LoginForm from "./auth/login/loginForm";
 
 export default function LoginPage() {
-	return <LoginForm />;
+	return (
+		<section className='w-full h-screen flex overflow-y-hidden'>
+			<div className='w-[60%] h-full xl:flex hidden'>
+				<div className="w-full h-full bg-[url('/assets/login-bg.jpeg')] bg-cover bg-center"></div>
+			</div>
+			<div className='xl:w-[40%] w-full bg-white flex items-center justify-center overflow-y-scroll'>
+				<div className='sm:w-[70%] w-[90%] flex flex-col items-start'>
+					<Image src={"/assets/logo.svg"} width={60} height={60} alt='logo' />
+					<h1 className='mt-3 sm:text-[32px] text-[24px] font-bold text-[#525252]'>
+						Login to your Account
+					</h1>
+					<p className='mt-1 text-base font-normal text-[#525252]'>
+						See what is going on with your Workboard
+					</p>
+					<LoginForm />
+				</div>
+			</div>
+		</section>
+	);
 }
